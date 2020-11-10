0e0943ad39d9a416c7b59eb95e7cc551e520a98c
sbaudoin <sylvain.baudoin@gmail.com>
2020-11-10 11:17:56 +0100

    Use lighter container

--- src/main/scripts/get_checks.sh
+++ src/main/scripts/get_checks.sh
@@ -2,4 +2,4 @@
 
 TARGET_DIR=org/sonar/l10n/shellcheck/rules/shellcheck
 mkdir -p `dirname $0`/../resources/$TARGET_DIR
-docker run --rm -v `pwd`/..:/mnt python:3.7-stretch sh /mnt/scripts/build_checks.sh /mnt/resources/$TARGET_DIR
+docker run --rm -v `pwd`/..:/mnt python:3.7-alpine3.12 sh /mnt/scripts/build_checks.sh /mnt/resources/$TARGET_DIR
